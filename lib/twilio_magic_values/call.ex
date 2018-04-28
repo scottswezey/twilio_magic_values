eca827425c4f6e9e8899a3b266c5040b995ddd4a
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-28 16:50:44 -0700

    added docs for magic calling numbers

--- lib/twilio_magic_values/call.ex
+++ lib/twilio_magic_values/call.ex
@@ -5,20 +5,56 @@ defmodule TwilioMagicNumbers.Call do
   See: [https://www.twilio.com/docs/iam/test-credentials#test-calls](https://www.twilio.com/docs/iam/test-credentials#test-calls)
   """
 
-  # Error 21212
+  @doc """
+  Returns the magic phone number which is **INVALID** to call from.
+
+  Expect the 21212 API error from Twilio.
+  """
   def from_invalid, do: "+15005550001"
+  @doc """
+  Returns the magic phone number which is **VALID** to call from.
+
+  Expect no error.
+  """
   def from_valid, do: "+15005550006"
-  # Not verified for account; Error 21210
+  @doc """
+  All other phone numbers, even ones owned by your production credentials, will return an error when used to call from.
+  (Other numbers are not verified on your test credentials.)
+
+  Expect the 21210 API error from Twilio.
+  """
+  def from_other(string)
   def from_other(num), do: num
 
-  # Error: 21217
+  @doc """
+  Returns the magic phone number which is **INVALID** to call to.
+
+  Expect the 21217 API error from Twilio.
+  """
   def to_invalid, do: "+15005550001"
-  # Error: 21214
+  @doc """
+  Returns the magic phone number which is **UNROUTABLE** to call to.
+
+  Expect the 21214 API error from Twilio.
+  """
   def to_cant_route, do: "+15005550002"
-  # Error: 21515
+  @doc """
+  Returns the magic phone number which has **GEO PERMISSION ERRORS** to call to.
+
+  Expect the 21515 API error from Twilio.
+  """
   def to_geo_permission_error, do: "+15005550003"
-  # Error 21216
+  @doc """
+  Returns the magic phone number which is **BLACKLISTED** to call to.
+
+  Expect the 21216 API error from Twilio.
+  """
   def to_blacklisted, do: "+15005550004"
-  # All other numbers are validated normally. Errors are dependent on input.
+  @doc """
+  All other phone numbers are potentially valid and subject to verified normally.
+
+  Errors are dependent on input.
+  """
+  def to_other(string)
   def to_other(num), do: num
 end
