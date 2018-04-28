bed9a8e06df1895b927183acb1ff434466635264
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-28 16:42:08 -0700

    mix format + docs completed

--- lib/twilio_magic_values/buy_number.ex
+++ lib/twilio_magic_values/buy_number.ex
@@ -4,11 +4,37 @@ defmodule TwilioMagicValues.BuyNumber do
 
   See: [https://www.twilio.com/docs/iam/test-credentials#test-incoming-phone-numbers](https://www.twilio.com/docs/iam/test-credentials#test-incoming-phone-numbers)
   """
+  @doc """
+  Returns the magic phone number which is **UNAVAILABLE** for purchase.
 
-  def unavailable, do: "+15005550000" # Error: 21421
-  def invalid,     do: "+15005550000" # Error: 21421
-  def valid,       do: "+15005550006"
+  Expect the 21421 API error from Twilio.
+  """
+  def unavailable, do: "+15005550000"
 
-  def empty_area_code,     do: "533" # Error: 21452
+  @doc """
+  Returns the magic phone number which is **INVALID** for purchase.
+
+  Expect the 21421 API error from Twilio.
+  """
+  def invalid, do: "+15005550000"
+
+  @doc """
+  Returns the magic phone number which is **VALID** for purchase.
+
+  Expect no error.
+  """
+  def valid, do: "+15005550006"
+
+  @doc """
+  Returns the magic area code which has **NO** numbers available for purchase.
+
+  Expect the 21452 API error from Twilio.
+  """
+  def empty_area_code, do: "533"
+  @doc """
+  Returns the magic area code which has numbers available for purchase.
+
+  Expect no error.
+  """
   def available_area_code, do: "500"
 end
