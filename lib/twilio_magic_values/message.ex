2896de66ff12dfe7a87a833acc03194bd62b8869
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-28 17:03:18 -0700

    added docs for messaging

--- lib/twilio_magic_values/message.ex
+++ lib/twilio_magic_values/message.ex
@@ -5,26 +5,81 @@ defmodule TwilioMagicValues.Message do
   See: [https://www.twilio.com/docs/iam/test-credentials#test-sms-messages](https://www.twilio.com/docs/iam/test-credentials#test-sms-messages)
   """
 
-  # Error: 21212
+  @doc """
+  Returns the magic phone number which is **INVALID** to send SMS from.
+
+  Expect the 21212 API error from Twilio.
+  """
   def from_invalid, do: "+15005550001"
-  # Error: 21606
+
+  @doc """
+  Returns the magic phone number which is **NOT VERIFIED BY YOUR ACCOUNT** to send SMS from.
+
+  Expect the 21606 API error from Twilio.
+  """
   def from_unowned, do: "+15005550007"
-  # Error: 21606
+
+  @doc """
+  Returns the magic phone number which is **CANT SEND SMS** to send SMS from.
+
+  Expect the 21606 API error from Twilio.
+  """
   def from_cant_sms, do: "+15005550007"
-  # Error: 21611
+
+  @doc """
+  Returns the magic phone number which has **A FULL QUEUE** to send SMS from.
+
+  Expect the 21611 API error from Twilio.
+  """
   def from_queue_full, do: "+15005550008"
+
+  @doc """
+  Returns the magic phone number which is **VALID** to send SMS from.
+
+  Expect no error.
+  """
   def from_valid, do: "+15005550006"
 
-  # Error: 21211
+  @doc """
+  Returns the magic phone number which is **INVALID** to send a message to.
+
+  Expect the 21211 API error from Twilio.
+  """
   def to_invalid, do: "+15005550001"
-  # Error: 21612
+
+  @doc """
+  Returns the magic phone number which is **UNROUTABLE** to send a message to.
+
+  Expect the 21612 API error from Twilio.
+  """
   def to_cant_route, do: "+15005550002"
-  # Error: 21408
+
+  @doc """
+  Returns the magic phone number which has a **GEO PERMISSION ERROR** to send a message to.
+
+  Expect the 21408 API error from Twilio.
+  """
   def to_geo_permission_error, do: "+15005550003"
-  # Error 21610
+
+  @doc """
+  Returns the magic phone number which is **BLACKLISTED** to send a message to.
+
+  Expect the 21610 API error from Twilio.
+  """
   def to_blacklisted, do: "+15005550004"
-  # Error 21614
+
+  @doc """
+  Returns the magic phone number which is **DOESNT SUPPORT SMS** to send a message to.
+
+  Expect the 21614 API error from Twilio.
+  """
   def to_cant_sms, do: "+15005550009"
-  # All other numbers are validated normally. Errors are dependent on input.
-  def other(num), do: num
+
+  @doc """
+  All other phone numbers are potentially valid and are verified normally.
+
+  Errors are dependent on input.
+  """
+  def to_other(string)
+  def to_other(num), do: num
 end
