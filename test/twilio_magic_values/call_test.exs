3572910c9aca7307fd95a19b67d467f3e5587dc8
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-29 12:21:27 -0700

    mix format

--- test/twilio_magic_values/call_test.exs
+++ test/twilio_magic_values/call_test.exs
@@ -6,44 +6,66 @@ defmodule TwilioMagicValues.CallTest do
   # POST /2010-04-01/Accounts/{TestAccountSid}/Calls
 
   test "from_invalid" do
-    results = make_call(from: Magic.from_invalid(),
-                          to: Magic.to_other("+17148675309"))
+    results =
+      make_call(
+        from: Magic.from_invalid(),
+        to: Magic.to_other("+17148675309")
+      )
 
     assert results.status == 400
     assert results.code == 21212
   end
-  test "from_valid" do
-    results = make_call(from: Magic.from_valid(),
-                          to: Magic.to_other("+17148675309"))
 
+  test "from_valid" do
+    results =
+      make_call(
+        from: Magic.from_valid(),
+        to: Magic.to_other("+17148675309")
+      )
 
     assert results.from_formatted == "(500) 555-0006"
     assert results.to_formatted == "(714) 867-5309"
   end
+
   test "to_invalid" do
-    results = make_call(from: Magic.from_valid(),
-                          to: Magic.to_invalid())
+    results =
+      make_call(
+        from: Magic.from_valid(),
+        to: Magic.to_invalid()
+      )
 
     assert results.status == 400
     assert results.code == 21217
   end
+
   test "to_cant_route" do
-    results = make_call(from: Magic.from_valid(),
-                          to: Magic.to_cant_route())
+    results =
+      make_call(
+        from: Magic.from_valid(),
+        to: Magic.to_cant_route()
+      )
 
     assert results.status == 400
     assert results.code == 21214
   end
+
   test "to_geo_permission_error" do
-    results = make_call(from: Magic.from_valid(),
-                          to: Magic.to_geo_permission_error())
+    results =
+      make_call(
+        from: Magic.from_valid(),
+        to: Magic.to_geo_permission_error()
+      )
 
     assert results.status == 400
     assert results.code == 21215
   end
+
   test "to_blacklisted" do
-    results = make_call(from: Magic.from_valid(),
-                          to: Magic.to_blacklisted())
+    results =
+      make_call(
+        from: Magic.from_valid(),
+        to: Magic.to_blacklisted()
+      )
 
     assert results.status == 400
     assert results.code == 21216
@@ -52,7 +74,9 @@ defmodule TwilioMagicValues.CallTest do
   defp make_call(from: f, to: t) do
     headers = []
     options = [hackney: [basic_auth: {api_test_sid(), api_auth_token()}]]
-    post_data = {:form, [{"To", t}, {"From", f}, {"Url", "http://demo.twilio.com/docs/voice.xml"}]}
+
+    post_data =
+      {:form, [{"To", t}, {"From", f}, {"Url", "http://demo.twilio.com/docs/voice.xml"}]}
 
     {:ok, %HTTPoison.Response{} = resp} = HTTPoison.post(api_url(), post_data, headers, options)
 
--- test/twilio_magic_values/message_test.exs
+++ test/twilio_magic_values/message_test.exs
@@ -6,74 +6,113 @@ defmodule TwilioMagicValues.MessageTest do
   # POST /2010-04-01/Accounts/{TestAccountSid}/Messages
 
   test "from_invalid" do
-    results = send_message(from: Magic.from_invalid(),
-                             to: Magic.to_other("+17148673509"))
+    results =
+      send_message(
+        from: Magic.from_invalid(),
+        to: Magic.to_other("+17148673509")
+      )
 
-     assert results.status == 400
-     assert results.code == 21212
+    assert results.status == 400
+    assert results.code == 21212
   end
+
   test "from_unowned" do
-    results = send_message(from: Magic.from_unowned(),
-                             to: Magic.to_other("+17148673509"))
+    results =
+      send_message(
+        from: Magic.from_unowned(),
+        to: Magic.to_other("+17148673509")
+      )
 
-     assert results.status == 400
-     assert results.code == 21606
+    assert results.status == 400
+    assert results.code == 21606
   end
+
   test "from_cant_sms" do
-    results = send_message(from: Magic.from_cant_sms(),
-                             to: Magic.to_other("+17148673509"))
+    results =
+      send_message(
+        from: Magic.from_cant_sms(),
+        to: Magic.to_other("+17148673509")
+      )
 
-     assert results.status == 400
-     assert results.code == 21606
+    assert results.status == 400
+    assert results.code == 21606
   end
+
   test "from_queue_full" do
-    results = send_message(from: Magic.from_queue_full(),
-                             to: Magic.to_other("+17148673509"))
+    results =
+      send_message(
+        from: Magic.from_queue_full(),
+        to: Magic.to_other("+17148673509")
+      )
 
-     assert results.status == 429
-     assert results.code == 21611
+    assert results.status == 429
+    assert results.code == 21611
   end
+
   test "from_valid" do
-    results = send_message(from: Magic.from_valid(),
-                             to: Magic.to_other("+17148673509"))
+    results =
+      send_message(
+        from: Magic.from_valid(),
+        to: Magic.to_other("+17148673509")
+      )
 
-     assert results.num_media == "0"
-     assert results.num_segments == "1"
+    assert results.num_media == "0"
+    assert results.num_segments == "1"
   end
+
   test "to_invalid" do
-    results = send_message(from: Magic.from_valid(),
-                             to: Magic.to_invalid())
+    results =
+      send_message(
+        from: Magic.from_valid(),
+        to: Magic.to_invalid()
+      )
 
-     assert results.status == 400
-     assert results.code == 21211
+    assert results.status == 400
+    assert results.code == 21211
   end
+
   test "to_cant_route" do
-    results = send_message(from: Magic.from_valid(),
-                             to: Magic.to_cant_route())
+    results =
+      send_message(
+        from: Magic.from_valid(),
+        to: Magic.to_cant_route()
+      )
 
-     assert results.status == 400
-     assert results.code == 21612
+    assert results.status == 400
+    assert results.code == 21612
   end
+
   test "to_geo_permission_error" do
-    results = send_message(from: Magic.from_valid(),
-                             to: Magic.to_geo_permission_error())
+    results =
+      send_message(
+        from: Magic.from_valid(),
+        to: Magic.to_geo_permission_error()
+      )
 
-     assert results.status == 400
-     assert results.code == 21408
+    assert results.status == 400
+    assert results.code == 21408
   end
+
   test "to_blacklisted" do
-    results = send_message(from: Magic.from_valid(),
-                             to: Magic.to_blacklisted())
+    results =
+      send_message(
+        from: Magic.from_valid(),
+        to: Magic.to_blacklisted()
+      )
 
-     assert results.status == 400
-     assert results.code == 21610
+    assert results.status == 400
+    assert results.code == 21610
   end
-  test "to_cant_sms" do
-    results = send_message(from: Magic.from_valid(),
-                             to: Magic.to_cant_sms())
 
-     assert results.status == 400
-     assert results.code == 21614
+  test "to_cant_sms" do
+    results =
+      send_message(
+        from: Magic.from_valid(),
+        to: Magic.to_cant_sms()
+      )
+
+    assert results.status == 400
+    assert results.code == 21614
   end
 
   defp send_message(from: f, to: t) do
