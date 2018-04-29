973ed8ed4ad27f0489c11ee9b521e8dd3ecbb034
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-29 11:02:32 -0700

    added URL comment for future use

--- test/twilio_magic_values/call_test.exs
+++ test/twilio_magic_values/call_test.exs
@@ -1,4 +1,6 @@
 defmodule TwilioMagicValues.CallTest do
   use ExUnit.Case
   doctest TwilioMagicValues.Call
+
+  # POST /2010-04-01/Accounts/{TestAccountSid}/Calls
 end
--- test/twilio_magic_values/message_test.exs
+++ test/twilio_magic_values/message_test.exs
@@ -1,4 +1,6 @@
 defmodule TwilioMagicValues.MessageTest do
   use ExUnit.Case
   doctest TwilioMagicValues.Message
+
+  # POST /2010-04-01/Accounts/{TestAccountSid}/Messages
 end
