eea210debf7cec60e1df7f8c12beee497e4ee234
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-29 11:01:15 -0700

    added libraries for testing

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule TwilioMagicValues.MixProject do
     [
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:httpoison, "~> 1.0", only: :test},
+      {:poison, "~> 3.1", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
