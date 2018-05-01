58af2a64fe6002ed1d8b0ec7b5f08198d818f9be
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-30 23:46:29 -0700

    added ex_coveralls config

--- mix.exs
+++ mix.exs
@@ -9,6 +9,10 @@ defmodule TwilioMagicValues.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
+      # Code coverage
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+
       # Docs / Hex
       name: "Twilio Magic Values",
       description:
