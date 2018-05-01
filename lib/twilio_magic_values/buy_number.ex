e5e159355f99241d23b7fa9617280bee30e85aee
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-30 21:03:00 -0700

    vastly improved doctests for BuyNumber module. regen docs

--- lib/twilio_magic_values/buy_number.ex
+++ lib/twilio_magic_values/buy_number.ex
@@ -8,6 +8,12 @@ defmodule TwilioMagicValues.BuyNumber do
   Returns the magic phone number which is **UNAVAILABLE** for purchase.
 
   Expect the [21422](https://www.twilio.com/docs/errors/21422) API error from Twilio.
+
+  ## Example
+
+      iex> map = buy_number(exactly: TwilioMagicValues.BuyNumber.unavailable_phone_number())
+      iex> Map.get(map, :code)
+      21422
   """
   def unavailable_phone_number, do: "+15005550000"
 
@@ -15,6 +21,12 @@ defmodule TwilioMagicValues.BuyNumber do
   Returns the magic phone number which is **INVALID** for purchase.
 
   Expect the [21421](https://www.twilio.com/docs/errors/21421) API error from Twilio.
+
+  ## Example
+
+      iex> map = buy_number(exactly: TwilioMagicValues.BuyNumber.invalid_phone_number())
+      iex> Map.get(map, :code)
+      21421
   """
   def invalid_phone_number, do: "+15005550001"
 
@@ -22,6 +34,14 @@ defmodule TwilioMagicValues.BuyNumber do
   Returns the magic phone number which is **VALID** for purchase.
 
   Expect no error.
+
+  ## Example
+
+      iex> map = buy_number(exactly: TwilioMagicValues.BuyNumber.valid_phone_number())
+      iex> Map.get(map, :code, "There was no error code")
+      "There was no error code"
+      iex> Map.get(map, :friendly_name)
+      "(500) 555-0006"
   """
   def valid_phone_number, do: "+15005550006"
 
@@ -29,6 +49,12 @@ defmodule TwilioMagicValues.BuyNumber do
   Returns the magic area code which has **NO** numbers available for purchase.
 
   Expect the [21452](https://www.twilio.com/docs/errors/21452) API error from Twilio.
+
+  ## Example
+
+      iex> map = buy_number(area_code: TwilioMagicValues.BuyNumber.empty_area_code())
+      iex> Map.get(map, :code)
+      21452
   """
   def empty_area_code, do: "533"
 
@@ -36,6 +62,14 @@ defmodule TwilioMagicValues.BuyNumber do
   Returns the magic area code which has numbers available for purchase.
 
   Expect no error, and the purchased phone number to be: +15005550006
+
+  ## Example
+
+      iex> map = buy_number(area_code: TwilioMagicValues.BuyNumber.available_area_code())
+      iex> Map.get(map, :code, "There was no error code")
+      "There was no error code"
+      iex> Map.get(map, :friendly_name)
+      "(500) 555-0006"
   """
   def available_area_code, do: "500"
 end
