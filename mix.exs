fdb5e57b6fe1f15773e724ac060a78a607b404d1
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-30 20:12:12 -0700

    tweaked docs config and regen docs

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule TwilioMagicValues.MixProject do
       name: "Twilio Magic Values",
       source_url: "https://github.com/scottswezey/twilio_magic_values",
       homepage_url: "https://scottswezey.github.io/twilio_magic_values",
-      docs: [main: "Readme", extras: ["README.md": [title: "Readme"]], output: "docs"]
+      docs: [main: "readme", extras: ["README.md": [title: "Readme"]], output: "docs"]
     ]
   end
 
