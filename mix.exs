9217946edca146fc927899aaae3ad50031be753f
Scott Swezey <scottswezey@users.noreply.github.com>
2018-04-30 23:43:54 -0700

    added code quality tools

--- mix.exs
+++ mix.exs
@@ -48,7 +48,9 @@ defmodule TwilioMagicValues.MixProject do
     [
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:httpoison, "~> 1.0", only: :test},
-      {:poison, "~> 3.1", only: :test}
+      {:poison, "~> 3.1", only: [:dev, :test]},
+      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.8", only: :test}
     ]
   end
 end
