2597dda29882c52a14aedf0c84531e42734331f6
Scott Swezey <scottswezey@users.noreply.github.com>
2018-05-13 16:52:33 -0700

    improved docs

--- lib/twilio_magic_values.ex
+++ lib/twilio_magic_values.ex
@@ -1,8 +1,11 @@
 defmodule TwilioMagicValues do
   @moduledoc false
 
+  @type phone_number :: String.t
+  @type t :: phone_number
+
   @doc """
-  Helper; Returns a URL to the Twilio documentation for a specific error code.
+  Helper returns a URL to the Twilio documentation for a specific error code.
 
   ## Examples
 
@@ -13,7 +16,7 @@ defmodule TwilioMagicValues do
       "https://www.twilio.com/docs/errors/21606"
 
   """
-  def error_info_url(integer_or_string)
+  def error_info_url(integer_or_string_error_code)
 
   def error_info_url(code) do
     "https://www.twilio.com/docs/errors/#{code}"
