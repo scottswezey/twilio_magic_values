1c5982a0e06491b7ecc0867005ecb2e5821a6c4d
Scott Swezey <scottswezey@users.noreply.github.com>
2018-10-01 19:06:47 -0700

    updated credo dep

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule TwilioMagicValues.MixProject do
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:httpoison, "~> 1.0", only: :test},
       {:poison, "~> 3.1", only: [:dev, :test]},
-      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.10.2", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.8", only: :test}
     ]
   end
