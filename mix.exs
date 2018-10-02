b64f387dbc8ba4c0e3faf607af82499ab3c93906
Scott Swezey <scottswezey@users.noreply.github.com>
2018-10-01 20:05:08 -0700

    bump version to 1.0.0-beta2 and update docs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TwilioMagicValues.MixProject do
   def project do
     [
       app: :twilio_magic_values,
-      version: "1.0.0-beta1",
+      version: "1.0.0-beta2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
